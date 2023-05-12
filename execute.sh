21f49b2f18962b2d0bb4f65ec957c46bcc629db3
Alex Escamilla <alex.escamilla@shokailab.com>
2023-05-12 14:37:15 -0500

    Initial commit

---
+++ execute.sh
@@ -0,0 +1,6 @@
+#!bash
+
+SHKL_JAR_APP_HOME=/home/alexescamilla/NetBeansProjects/ReporteArticulos/target
+SHKL_JAVA_HOME=/home/alexescamilla/Development/java/zulu17-34-19/bin
+
+$SHKL_JAVA_HOME/java -jar $SHKL_JAR_APP_HOME/ReporteArticulos-1.0.jar
