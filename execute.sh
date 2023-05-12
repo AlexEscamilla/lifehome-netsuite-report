c9d84f00d5c064b29305eaab0db39c528b5ba055
Alex Escamilla <alex.escamilla@shokailab.com>
2023-05-12 17:33:35 -0500

    update java folder

--- execute.sh
+++ execute.sh
@@ -1,7 +1,7 @@
 #!bash
 
 REPORT_HOME=$HOME/ReportesNetsuite
-REPORT_HOME_JAVA=$REPORT_HOME/java/zulu17.42.19-ca-jdk17.0.7-macosx_aarch64/bin
+REPORT_HOME_JAVA=$REPORT_HOME/java/zulu17/bin
 REPORT_HOME_REPORTE_ARCHIVOS=$REPORT_HOME/ReporteArchivos
 
 $REPORT_HOME_JAVA/java -jar $REPORT_HOME_REPORTE_ARCHIVOS/ReporteArticulos-1.0.jar
